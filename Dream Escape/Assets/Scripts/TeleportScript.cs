28626ca886007abf435f15b403cafaf2177e9058
mtayag0731 <mtayag@csumb.edu>
2022-11-30 23:03:05 -0800

    Minigame

---
+++ Dream Escape/Assets/Scripts/TeleportScript.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class TeleportScript : MonoBehaviour
+{
+    public GameObject character;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.Equals(character))
+        {
+            SceneManager.LoadScene("SampleScene");
+        }
+    }
+}
